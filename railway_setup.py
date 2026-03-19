9fc7f42d38f5fbf31bd0817caa66248fd22d3cc9
Swastik Gautam <swastikgautam76@gmail.com>
2026-03-19 17:22:23 +0530

    Auto load commands on deploy

--- railway_setup.py
+++ railway_setup.py
@@ -9,6 +9,14 @@ django.setup()
 from django.core.management import call_command
 call_command('migrate', '--run-syncdb')
 
+# Load commands
+from hardware.models import CommandLog
+
+if CommandLog.objects.count() == 0:
+    print("Loading commands...")
+    exec(open('load_commands.py').read())
+
+
 from hardware.models import CustomUser
 if not CustomUser.objects.filter(username='admin').exists():
     u = CustomUser.objects.create_superuser('admin', 'admin@dainikbhaskar.com', 'admin123')
